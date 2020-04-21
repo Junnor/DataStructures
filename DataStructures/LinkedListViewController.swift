7cfc0aba4e04f12ee0ad0b5f60c92af3a43d9732
Dengquan <dengquanzhu@gmail.com>
2020-04-21 22:50:42 +0800

    添加一些链表的测试题目

--- DataStructures/LinkedListViewController.swift
+++ DataStructures/LinkedListViewController.swift
@@ -170,22 +170,263 @@ class LinkedListViewController: UIViewController {
             print("list1: \(list1)")
             print("list2: \(list2)")
             
-            print("List1 uniquely referenced: \(isKnownUniquelyReferenced(&list1.head))")
             print("After add 3 to list2")
             list2.append(3)
-            print("List1 uniquely referenced: \(isKnownUniquelyReferenced(&list1.head))")
+            print("list1: \(list1)")
+            print("list2: \(list2)")
+            
+            print("Removing node at middle ")
+            if let node = list2.node(at: 0) {
+                print("Can remove....")
+                list2.remove(after: node)
+            }
+            print("List2: \(list2)")
+        }
+    }
+    
+    private func linkedListShareNodesTest() {
+        example(of: "linked list node share") {
+            var list1 = LinkedList<Int>()
+            (1...3).forEach { list1.append($0) }
+            var list2 = list1
+
+            list2.push(0)
+            print("list1: \(list1)")
+            print("list2: \(list2)")
+            
+            list1.push(100)
             print("list1: \(list1)")
             print("list2: \(list2)")
         }
     }
+    
+    private func chalenge1() {
+        example(of: "linked list chalenge 1") {
+            var list = LinkedList<Int>()
+            (1...3).forEach { list.append($0) }
+            print("list: \(list)")
+            printInReverse(list)
+        }
+    }
 
+    private func chalenge2() {
+        example(of: "linked list chalenge 2") {
+            var list = LinkedList<Int>()
+            (1...4).forEach { list.append($0) }
+            print("Original list: \(list)")
+            if let middleNode = getMiddle(list) {
+                print("Middle list: \(middleNode)")
+            }
+        }
+    }
 
+    
+    private func chalenge3() {
+        example(of: "linked list chalenge 3") {
+            var list = LinkedList<Int>()
+            (1...3).forEach { list.append($0) }
+            print("Original list: \(list)")
+            list.reverse()
+            print("After reverse list: \(list)")
+        }
+    }
+
+    private func chalenge4() {
+        example(of: "linked list chalenge 4") {
+            var list1 = LinkedList<Int>()
+            list1.append(-1)
+            list1.append(2)
+            list1.append(3)
+            list1.append(6)
+            
+            var list2 = LinkedList<Int>()
+            list2.append(1)
+            list2.append(4)
+            list2.append(10)
+            list2.append(11)
+            
+            print("List1: \(list1)")
+            print("List2: \(list2)")
+            
+            let merge = mergeList(list1, with: list2)
+            print("MergeList: \(merge)")
+        }
+    }
+
+    private func chalenge5() {
+        example(of: "linked list chalenge 5") {
+            var list = LinkedList<Int>()
+            list.append(1)
+            list.append(3)
+            list.append(3)
+            list.append(4)
+            
+            print("Before removing list: \(list)")
+            list.removeAll(3)
+            print("After removing list: \(list)")
+        }
+    }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        linkedListLinkedListCOWTest()
+        chalenge5()
     }
 
+}
+
+
+
+extension LinkedListViewController {
+    
+    // Chalenge 1
+    private func printInReverse<T>(_ list: LinkedList<T>) {
+        printInReverse(list.head)
+    }
+    
+    private func printInReverse<T>(_ node: Node<T>?) {
+        guard let node = node else {
+            return
+        }
+        printInReverse(node.next)
+        print(node.value)
+    }
+    
+    // Chalenge 2
+    private func getMiddle<T>(_ list: LinkedList<T>) -> Node<T>? {
+        var slow = list.head
+        var fast = list.head
+        while let nextFast = fast?.next {
+            fast = nextFast.next
+            slow = slow?.next
+        }
+        return slow
+    }
+    
+    // Chalenge 4
+    private func mergeList<T: Comparable>(_ left: LinkedList<T>, with right: LinkedList<T>) -> LinkedList<T> {
+        
+        // 判断是否有空链表
+        if left.isEmpty {
+            return right
+        }
+        
+        if right.isEmpty {
+            return left
+        }
+        
+        var newHead: Node<T>?
+        
+        // 找到起始节点
+        var tail: Node<T>?
+        var currentLeft = left.head
+        var currentRight = right.head
+        
+        if let leftNode = currentLeft, let rightNode = currentRight {
+            if leftNode.value < rightNode.value {
+                newHead = leftNode // 设置新头部
+                currentLeft = leftNode.next
+            } else {
+                newHead = rightNode // 设置新头部
+                currentRight = rightNode.next
+            }
+            tail = newHead
+        }
+        
+        // 遍历一遍，此时会有一个链表便利完
+        while let leftNode = currentLeft, let rightNode = currentRight {
+            if leftNode.value < rightNode.value {
+                tail?.next = leftNode
+                currentLeft = leftNode.next
+            } else {
+                tail?.next = rightNode
+                currentRight = rightNode.next
+            }
+            tail = tail?.next
+        }
+        
+        // 把某个剩余的链表遍历完
+        if let leftNodes = currentLeft {
+            tail?.next = leftNodes
+        }
+        
+        if let rightNodes = currentRight {
+            tail?.next = rightNodes
+        }
+
+        // 创建一个新的链表
+        var newList = LinkedList<T>()
+        newList.head = newHead
+        newList.tail = {
+            while let next = tail?.next {
+                tail = next
+            }
+            return tail
+        }()
+
+        return newList
+    }
+    
+    // Chalenge 5
+    private func removeOccurence<T: Comparable>(_ value: T, in list: LinkedList<T>) -> LinkedList<T> {
+        // 如果需要移除头部相等元素
+        var head = list.head
+        while let newHead = head, newHead.value == value {
+            head = newHead.next
+        }
+        
+    
+        var newList = list
+
+        var current = newList.head
+        var prev = newList.head
+        
+        while current != nil {
+            if current!.value == value {
+                if current === newList.head {
+                    current = current?.next
+                    prev = current?.next
+                    newList.head = current // reset head
+                } else {
+                    current = current?.next
+                    prev?.next = current
+                    
+                    if current == nil {
+                        newList.tail = prev // reset tail
+                    }
+                }
+            } else {
+                prev = current
+                current = current?.next
+            }
+        }
+        return newList
+    }
+
+    
+}
+
+
+public class Node<Value> {
+    
+    var value: Value
+    var next: Node?
+    
+    init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+    
+}
+
+extension Node: CustomStringConvertible {
+    
+    public var description: String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        return "\(value) -> " + "\(String(describing: next))" + " "
+    }
+    
 }
 
 public struct LinkedList<Value> {
@@ -285,7 +526,9 @@ public struct LinkedList<Value> {
     
     @discardableResult
     public mutating func remove(after node: Node<Value>) -> Value? {
-        copyNodes()
+        guard let node = copyNodes(returningCopyOf: node) else {
+            return nil
+        }
         
         defer {
             if node.next === tail {
@@ -297,13 +540,14 @@ public struct LinkedList<Value> {
     }
     
     // 从头到尾复制一份
-    private mutating func copyNodes() {
+    @discardableResult
+    private mutating func copyNodes() -> Bool {
         guard !isKnownUniquelyReferenced(&head) else {
-            return
+            return false
         }
         
         guard var oldNode = head else {
-            return
+            return false
         }
         
         head = Node(value: oldNode.value)
@@ -317,8 +561,35 @@ public struct LinkedList<Value> {
         }
         
         tail = newNode
+        return true
     }
     
+    private mutating func copyNodes(returningCopyOf node: Node<Value>?) -> Node<Value>? {
+        guard !isKnownUniquelyReferenced(&head) else {
+            return nil
+        }
+        
+        guard var oldNode = head else {
+            return nil
+        }
+        
+        head = Node(value: oldNode.value)
+        var newNode = head
+        var copyNode: Node<Value>?
+        
+        while let nextOldNode = oldNode.next {
+            if oldNode === node {
+                copyNode = newNode
+            }
+            newNode?.next = Node(value: nextOldNode.value)
+            newNode = newNode?.next
+            
+            oldNode = nextOldNode
+        }
+                
+        return copyNode
+    }
+
 }
 
 extension LinkedList: CustomStringConvertible {
@@ -333,30 +604,6 @@ extension LinkedList: CustomStringConvertible {
     
 }
 
-
-
-public class Node<Value> {
-    
-    var value: Value
-    var next: Node?
-    
-    init(value: Value, next: Node? = nil) {
-        self.value = value
-        self.next = next
-    }
-    
-}
-
-extension Node: CustomStringConvertible {
-    
-    public var description: String {
-        guard let next = next else {
-            return "\(value)"
-        }
-        return "\(value) -> " + "\(String(describing: next))" + " "
-    }
-    
-}
 
 extension LinkedList: Collection {
     
@@ -402,4 +649,55 @@ extension LinkedList: Collection {
         return position.node!.value
     }
         
+}
+
+
+extension LinkedList {
+    
+    // Chalenge 3
+    mutating func reverse() {
+        tail = head
+        
+        var prev = head
+        var current = head?.next
+        prev?.next = nil
+        
+        while current != nil {
+            let next = current?.next
+            current?.next = prev
+            prev = current
+            current = next
+        }
+        
+        head = prev
+    }
+
+}
+
+extension LinkedList where Value: Equatable {
+    
+    // Chalenge 5
+    mutating func removeAll(_ value: Value) {
+        // 如果必要移除头部
+        while let head = self.head, head.value == value {
+            self.head = head.next
+        }
+        
+        // 遍历移除
+        var prev = head
+        var current = head?.next
+        while let currentNode = current {
+            guard currentNode.value != value else {
+                prev?.next = currentNode.next
+                current = prev?.next
+                continue
+            }
+            prev = current
+            current = current?.next
+        }
+        
+        // 设置尾部
+        tail = prev
+    }
+    
 }
